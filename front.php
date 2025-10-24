8f9ea5dfe7f1602f876468a9901e57645b00ec43
starship-droid <github@sophieallen.space>
2025-10-24 22:23:03 +0000

    Goodness Triangle single sided

--- front.php
+++ front.php
@@ -1,9 +1,14 @@
-	<section>
+<section>
 		<div class="qrcode">
-			<img src="qrcode.png" alt="QR Code" />
+			<img src="qrcode.svg" alt="QR Code" />
 		</div>
 		<div class="content social">
-			@MrAlexar
+			<div class="logo" >
+				<svg width="20" height="20" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.9624 83.619C31.102 44.7195 60.1399 8.17071 68.5548 10.071C84.1413 13.5908 98.0687 82.1696 84.5021 88.3525C72.1317 93.99 38.9849 85.5806 10 56.592" stroke="#367D77" stroke-width="10"/>
+</svg>
+			</div>
+			<div class="brand">Goodness Triangle</div>
 		</div>
 		<div class="content info">
 			<h1>Alexar Pendashteh</h1>
@@ -11,10 +16,7 @@
 				+61 410 7220 54
 			</div>
 			<div class="email">
-				hi@alexar.me
+				alexar@goodnesstriangle.com
 			</div>
-		</div>
-		<div class="git">
-			$ git clone git@github.com:mralexar/vcard.git
 		</div>
 	</section>
